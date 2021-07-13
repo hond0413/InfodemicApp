bafe1ba6ccd4925b4db3f45a8b6e616730cd4127
hond0413 <hondsoccer0413@gmail.com>
2021-07-13 20:21:47 +0900

    initial commit

---
+++ infodemicapp/src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-app >
+    <!-- <v-app-bar color="#242a3c" class="appBar" elevate-on-scroll>
+      <v-app-bar-nav-icon @click="menue = !menue" color="white"></v-app-bar-nav-icon>
+      <v-toolbar-title class="toolbarTitle">
+        Infodemic App
+      </v-toolbar-title>
+    </v-app-bar>
+    <v-navigation-drawer v-model="menue" fixed temporary>
+      <v-list nav dense>
+        <v-list-item-group>
+          <v-list-item>
+            <v-list-item-title>HOME</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer> -->
+    <h1 style="color: white; margin: auto;">
+      Covid19 Observatory
+    </h1>
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+
+  components: {
+  },
+
+  data: () => ({
+    menue: false
+  }),
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap');
+.v-application{
+  margin: 0 auto; 
+  width: 100%;
+  /* max-width: 640px; */
+  background-color: #061a2b !important;
+  font-family: 'Noto Sans JP', sans-serif !important; 
+}
+*{
+  /* color: #fafafa !important; */
+}
+body{
+  background: #061a2b;
+}
+.appBar{
+  max-height: 56px;
+}
+.toolbarTitle{
+  color: white;
+}
+</style>
